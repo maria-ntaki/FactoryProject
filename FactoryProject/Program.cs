1aafd9b8c95afae4229d5a69ec4b128eb794d501
JohnLez <jeanlezineau@gmail.com>
2019-12-27 08:48:54 +0200

    ++Sample ui to implement

--- FactoryProject/Program.cs
+++ FactoryProject/Program.cs
@@ -10,7 +10,35 @@ namespace FactoryProject
     {
         static void Main(string[] args)
         {
-            //Testing changes v0.1
+            Console.WriteLine("Welcome");
+
+            Console.WriteLine("Which entity would you like to work with?");
+
+            Console.WriteLine("1. Factory");
+            //Prints a list of all factories
+            //Prompts user to bind an element(factory) to work with
+            //Prints all data for specific factory
+            //Option to "force" rawMaterial resupply 
+            //Option to "force" chocolate production
+            Console.WriteLine("2. Organiastion");
+            //Prints full organization info
+            //Option to print List of all contracts
+            //Option to print List of all factories
+            //Option to print List of all stores
+            //Option to print List of all contracts
+            Console.WriteLine("3. Store");
+            //Prints a list of all stores
+            //Prompts user to bind an element(store) to work with
+            //Prints all data for specific store
+            //Option to "force" chocolate resupply 
+            
+            Console.WriteLine("4. Customer");
+            //Prints all customers or creates one
+            //Binds one
+            //Forms an order
+            //Prints all stores
+            //Asks which store to make the order from
+            //done
         }
     }
 }
